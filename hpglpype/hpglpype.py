23c7ebbc6a0e5f9f50479a8a93b6d768cf824049
zxsq <git@zxsq.cc>
2020-08-24 07:52:04 -0400

    🚧 plotter units
    conversion to plotter units added

--- hpglpype/hpglpype.py
+++ hpglpype/hpglpype.py
@@ -2,6 +2,7 @@ from typing import Tuple
 
 import click
 import vpype as vp
+import numpy as np
 
 
 @click.command()
@@ -19,6 +20,10 @@ import vpype as vp
 @click.option(
     "-c", "--center", is_flag=True, help="Center the geometries within the SVG bounds.",
 )
+
+
+
+
 @vp.global_processor
 def hpgl(
     vector_data: vp.VectorData,
@@ -33,13 +38,34 @@ def hpgl(
 
     # TODO: <your code here - call 800-555-1212>
     #print(vector_data.ids())
-    usedLayers = list(vector_data.ids()) 
+    ##usedLayers = list(vector_data.ids()) 
     #print(usedLayers)
     ###todo| layer ids to pen #s
     ###    | for layer i get lines
     ###    | |sel pen i
     ###    | |convert to plotter units
     ###    | |write as pu/pd commands
+    
+    def plotUnit(line):
+    #convert vpype pixels (96ppi) to plotter units (0.025mm)
+    #[0.025 mm/pu] / [25.4 mm/in] = 0.0009842519685 in/pu
+    #1 in / 0.0009842519685 in/pu = 1,016.0000000041 pu/in
+    #1,016.0000000041 pu/in / [96 px/in] = 10.5833333334 pu/px
+    ##plotter truncates to int when using pu
+    ##px val * 10.583333 = pu val
+        PUConstant = 10.583333
+        PUX = line.real * PUConstant
+        PUY = line.imag * PUConstant
+        print("x", PUX, "y", PUY)
+        return(0)
+
+    for layer_id in sorted(vector_data.layers.keys()):
+        layer = vector_data.layers[layer_id]
+        print("layer", layer)
+        for line in layer:
+            print("line ", line)
+            plotUnit(line)
+            
     
 
     return vector_data
